3baa563ca35dafafb95194a9bc2c7e1b168420ae
Ivy Chang <ivycwq@icloud.com>
2016-10-29 21:11:55 -0700

    extending shape class

--- TwoDimensionalShape.cs
+++ TwoDimensionalShape.cs
@@ -1,5 +1,5 @@
 namespace Shape {
-  public abstract class TwoDimensionalShape {
+  public abstract class TwoDimensionalShape : Shape {
     public TwoDimensionalShape() : base(){}
     public abstract double GetArea();
   }   
