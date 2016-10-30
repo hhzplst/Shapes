556f3f761c6065db99bccfde41c46296a9781cd8
Ivy Chang <ivycwq@icloud.com>
2016-10-29 21:37:53 -0700

    adding abstract methods

--- ThreeDimensionalShape.cs
+++ ThreeDimensionalShape.cs
@@ -1,5 +1,7 @@
 namespace Shape {
   public abstract class ThreeDimensionalShape : Shape {
     public ThreeDimensionalShape() : base(){}
+    public abstract double GetArea();
+    public abstract double GetVolume();
   }
 }
