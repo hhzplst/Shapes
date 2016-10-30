bc06bd488be3042fedff6ed3788044b31157a9ae
Ivy Chang <ivycwq@icloud.com>
2016-10-29 21:25:47 -0700

    updating test case

--- ShapeTest.cs
+++ ShapeTest.cs
@@ -5,14 +5,15 @@ namespace Shape {
     public static void Main(string[] args) {
       Shape[] shapes = new Shape[]{
         new Point(1,2,3),
-        new Line(new Point(0,0,0), new Point(4,4,4))
+        new Line(new Point(0,0,0), new Point(4,4,4)),
+        new Circle(new Point(3,3,3), 3)
       };
       foreach (Shape s in shapes) {
-        Console.WriteLine("*********************************");
         Console.WriteLine(s);
         s.Move(2,2,2);
         Console.WriteLine("Moving x, y, z coordinates by 2...");
         Console.WriteLine(s);
+        Console.WriteLine("\n");
       }
     }
   }
