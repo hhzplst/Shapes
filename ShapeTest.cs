fe9cdff4709012ba6a238bed98bc19410a30c024
Ivy Chang <ivycwq@icloud.com>
2016-10-29 21:38:14 -0700

    adding new test cases

--- ShapeTest.cs
+++ ShapeTest.cs
@@ -6,11 +6,12 @@ namespace Shape {
       Shape[] shapes = new Shape[]{
         new Point(1,2,3),
         new Line(new Point(0,0,0), new Point(4,4,4)),
-        new Circle(new Point(3,3,3), 3)
+        new Circle(new Point(3,3,3), 3),
+        new Sphere(new Point(5,5,5), 2)
       };
       foreach (Shape s in shapes) {
         Console.WriteLine(s);
-        s.Move(2,2,2);
+        s.Move(1,4,7);
         Console.WriteLine("Moving x, y, z coordinates by 2...");
         Console.WriteLine(s);
         Console.WriteLine("\n");
